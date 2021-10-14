2a2376a801c1cf3b17790fa161c0def3090f94fe
UmarkhonAkhmedov <umarkhonakhmedov2000@gmail.com>
2021-10-14 21:37:46 +0500

    Learning new things in Java

--- Java Tutorial/First.java
+++ Java Tutorial/First.java
@@ -1,5 +1,46 @@
+import java.util.Date;
+import java.awt.*;
+
+
+// public class First {
+//   public static void main(String[] args) {
+//     System.out.println("Hello World");
+//   }
+// }
+
 public class First {
-  public static void main(String[] args) {
-    System.out.println("Hello World");
+  public static void main(String[] args){
+    // int age = 30;
+    // int myAge = 30;
+    // int herAge = myAge;
+    // System.out.println(age);
+    // System.out.println(herAge);
+
+    ////////////////////////////////////////////
+    // byte age = 30;
+    // long viewsCount = 3_123_456_789L;
+    // float price = 10.99F;
+    // char letter = 'A';
+    // boolean isEligible = true;
+
+    ////////////////////////////////////////////
+    // byte age = 30;
+    // Date now = new Date();
+    // System.out.println(now);
+
+    ////////////////////////////////////////////
+    // Point point1 = new Point(x:1, y:1);
+    // Point point2 = point1;
+    // point1.x = 2;
+    // System.out.println(point2);
+
+    ////////////////////////////////////////////
+    String message = "Hello World" + "!!";
+    message.endsWith("!!");
+    System.out.println(message.length());
+    System.out.println(message.indexOf("e"));
+    System.out.println(message.replace("!", "*"));
+    System.out.println(message.replace("!", "*"));
+     
   }
 }
